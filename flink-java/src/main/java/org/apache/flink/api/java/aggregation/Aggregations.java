260301c305566cfab0bcd3e50253bde4804b3458
Jacob Maloney <jmaloney@conversantmedia.com>
2015-05-15 13:59:50 -0500

    fixed avg aggregation

--- flink-java/src/main/java/org/apache/flink/api/java/aggregation/Aggregations.java
+++ flink-java/src/main/java/org/apache/flink/api/java/aggregation/Aggregations.java
@@ -25,7 +25,8 @@ public enum Aggregations {
 	
 	SUM (new SumAggregationFunction.SumAggregationFunctionFactory()),
 	MIN (new MinAggregationFunction.MinAggregationFunctionFactory()),
-	MAX (new MaxAggregationFunction.MaxAggregationFunctionFactory());
+	MAX (new MaxAggregationFunction.MaxAggregationFunctionFactory()),
+	AVG (new AvgAggregationFunction.AvgAggregationFunctionFactory());
 	
 	// --------------------------------------------------------------------------------------------
 	
